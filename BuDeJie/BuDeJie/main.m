6f2fed34acde6cd4c011e4ee7a6c20a5ca15ae3c
@shlyren <shlyren@163.com>
2016-03-31 16:00:01 +0800

    初始化项目

---
+++ BuDeJie/BuDeJie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BuDeJie
+//
+//  Created by 任玉祥 on 16/3/31.
+//  Copyright © 2016年 任玉祥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
